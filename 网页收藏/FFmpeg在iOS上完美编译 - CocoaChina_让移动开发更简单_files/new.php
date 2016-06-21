d7b4f9de291d44cf4f4f78f2708f5d52fd8c2ce1
skeyboy <lylapp@163.com>
2016-06-21 10:20:41 +0800

    网页收藏

---
+++ 网页收藏/FFmpeg在iOS上完美编译 - CocoaChina_让移动开发更简单_files/new.php
@@ -0,0 +1,9 @@
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685878' target='_blank'>绘图时好卡，我用真机测试了下内存和CPU，发现内存用的很少，CPU占了85%</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685876' target='_blank'>app store上架官方文档</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685875' target='_blank'>iTunes Connect 提示阅读新的条款。 点击去啥都没有。 每次登录都提示 。</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685874' target='_blank'>想实现微信小视频的效果，但是TableView同时播放多个视频，滚动时候特别卡</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685873' target='_blank'>上班无聊，一起来讨论讨论iOS的问题</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685872' target='_blank'>做应用开发感觉没前途啊iOS</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685871' target='_blank'>自定义聊天界面，结合绑定环信IM的聊天服务</a></li>");
+document.write("<li><a href='http://www.cocoachina.com/bbs/read.php?tid=1685869' target='_blank'>异步获取数据赋值后无法调用，是否需要延时执行？</a></li>");
+document.write("<li class='clearfix noborder-b'><a href='http://www.cocoachina.com/bbs/read.php?tid=1685868' target='_blank'>是否能获取ios正在后台播放的歌曲的信息</a></li>");
